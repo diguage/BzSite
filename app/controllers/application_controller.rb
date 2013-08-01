573b57b8bd62ac66804b79c9f5f10c9152cbd2a3
diguage <leejun119@gmail.com>
2013-08-01 10:19:12 +0000

    添加footer中的最新产品

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :get_latest_products
+  
+  def get_latest_products(num = 4)
+    @latest_products ||= Product.order(created_at: :desc).limit(num)
+  end
 end
